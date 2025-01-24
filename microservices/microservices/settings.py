e55626d0820c32fd5d9e5dd28aee8eb840d0191b
fredzxc <johnfredericktsabondo@iskolarngbayan.pup.edu.ph>
2025-01-24 11:28:04 +0800

    ready3

--- microservices/microservices/settings.py
+++ microservices/microservices/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1').split(',')
 
 # API KEYS
 PAYMONGO_SECRET_KEY = config("PAYMONGO_SECRET_KEY")
